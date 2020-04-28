be7526fa3f7b5b104100fcb548959999e9699bb9
Anthony <anthony.smith1760@gmail.com>
2020-04-28 13:18:15 -0400

    Final Changes

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const cover = document.getElementById('cover');
 
 
 //Song titles
-const songs = ['hey', 'summer', 'ukulele'];
+const songs = ['Sunrise', 'Promises', 'Things Change'];
 
 //Keep track of song
 let songIndex = 2;
@@ -45,6 +45,51 @@ function pauseSong() {
     audio.pause();
 }
 
+//Previous song
+function prevSong() {
+    songIndex--;
+
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+//Next song
+function nextSong() {
+    songIndex++;
+
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+//Update progress bar
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+}
+
+//Set progress bar
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+    
+
+    audio.currentTime = (clickX / width) * duration;
+}
+
+
+
 //Event Listener
 playBtn.addEventListener('click', () => {
     const isPlaying = musicContainer.classList.contains('play');
@@ -55,3 +100,17 @@ playBtn.addEventListener('click', () => {
         playSong();
     }
 });
+
+//Change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+
+//Time/song update
+audio.addEventListener('timeupdate', updateProgress);
+
+//Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio. addEventListener('ended', nextSong);
